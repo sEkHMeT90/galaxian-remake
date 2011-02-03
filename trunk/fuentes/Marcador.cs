3e834342065faaed3de89d46fe76ca1cf9db6a69
mikymal6 <mikymal6@25849b62-e80a-09ad-24df-e5005423cf44>
2011-02-03 19:31:59 +0000

    marcador muestra vidas i niveles

--- trunk/fuentes/Marcador.cs
+++ trunk/fuentes/Marcador.cs
@@ -19,25 +19,91 @@
 
 public class Marcador
 {
+    private ElemGrafico imagenVidas;
+    private int vidas;
+    private ElemGrafico imagenNivel;
+    private int nivel;
+    //private ElemGrafico Bandera;
+        
+    private int puntuacion;
+    private int mejorPunt;
+    
+    
+        //private int Banderas;
+    
+
+    private Partida miPartida;
+    Fuente tipoDeLetra;
+
+    public Marcador(Partida p)
+    {
+        miPartida = p;
+        tipoDeLetra = new Fuente("FreeSansBold.ttf", 18);
+    }
+
 
     public void IndicarPuntos(int p)
     {
-
+        puntuacion = p;
     }
+
 
     public void IndicarNivel(int n)
     {
-
+        nivel = n;
     }
 
-    public void IndicarVidas()
-    {
 
+    public void IndicarVidas(int v)
+    {
+        vidas = v;
     }
 
 
     public  void DibujarOculta()
     {
+        imagenVidas = new ElemGrafico("imagenes/vida.png");
+        imagenNivel = new ElemGrafico("imagenes/bandera.png");
+        //Banderas = new ElemGrafico("imagenes/bandera.png");
 
+        Hardware.EscribirTextoOculta("Puntos: ",
+            /* Coordenadas */ 100, 100,  /* Colores */ 50, 255, 50,
+            tipoDeLetra);
+        Hardware.EscribirTextoOculta(System.Convert.ToString(puntuacion),
+            /* Coordenadas */ 450, 550, /* Colores */ 50, 255, 50,
+            tipoDeLetra);
+
+        Hardware.EscribirTextoOculta("Maximo: ",
+            /* Coordenadas */ 350, 570,  /* Colores */ 50, 255, 50,
+            tipoDeLetra);
+        Hardware.EscribirTextoOculta(System.Convert.ToString(mejorPunt),
+            /* Coordenadas */ 400, 100, /* Colores */ 50, 255, 50,
+            tipoDeLetra);
+            
+        
+        // Vidas, como barra (maximo 10 segmentos)
+        int navecitas = vidas;
+        if (navecitas > 5) navecitas = 5;
+        for (int i = 0; i < navecitas; i++)
+        {
+            Vidas.DibujarOculta(680 + 20 * i, 550);
+        }
+
+        Hardware.EscribirTextoOculta("Vidas",
+            /* Coordenadas */ 100, 570,  /* Colores */ 50, 255, 50,
+            tipoDeLetra);
+
+        // Banderas,(maximo 10 segmentos)
+        int banderitas = nivel;
+        if (banderitas > 10)banderitas = 0;
+            //Bandera = 1;
+        for (int i = 0; i < banderitas; i++)
+        {
+            banderitas.DibujarOculta(150 + 20 * i, 350);
+        }
+
+        Hardware.EscribirTextoOculta("banderitas",
+            /* Coordenadas */ 750, 570,  /* Colores */ 50, 255, 50,
+            tipoDeLetra);
     }
 } /* fin de la clase Marcador */
