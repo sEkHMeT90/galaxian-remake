c1d15d4444f9126ac34caa7344175d2e50778a30
andresmarotta@gmail.com <andresmarotta@gmail.com@25849b62-e80a-09ad-24df-e5005423cf44>
2011-02-03 19:14:38 +0000

    Array de enemigos en flota.

--- trunk/fuentes/Flota.cs
+++ trunk/fuentes/Flota.cs
@@ -14,34 +14,66 @@
    ---------------------------------------------------
    0.01  02-Feb-2011  Nacho Cabanes
                       Version inicial: esqueleto vacio
+   0.02  03-Feb-2011  Andrés Marotta, Pedro Zalacain
+                      Array de enemigos, 
  ---------------------------------------------------- */
 
 
 public class Flota
 {
-    EnemAzul enemigo1;
+    private int maxEnemigos = 10;
+    private Enemigo[] enemigos;
+    // private EnemAzul enemigo1;
+    
     public Flota()
     {
-        enemigo1 = new EnemAzul();
+        enemigos = new Enemigo[maxEnemigos];
+
+        for (int i = 0; i < maxEnemigos; i++)
+            enemigos[i] = new Enemigo();
+
+        // enemigo1 = new EnemAzul();
     }
 
 
     // Mueve todos los enemigos en bloque y (quiza) lanza ataques
-    public  void Mover()
+    public void Mover()
     {
-
+        /*
+        for (int i = 0; i < maxEnemigos; i++)
+            enemigos[i].DibujarOculta();
+        Random r = new Random();
+        */
     }
 
     // Lanza uno o varios enemigos al ataque
     public void Atacar()
     {
-
+        /*
+        Random r = new Random();
+        */
     }
-
 
     // Dibuja todos los enemigos
     public  void DibujarOculta()
     {
-        enemigo1.DibujarOculta();
+        for (int i = 0; i < maxEnemigos; i++)
+            enemigos[i].DibujarOculta(33*i,5);
+        // enemigo1.DibujarOculta();
     }
+
+    // Mueve todos los enemigos en bloque y (quiza) lanza ataques
+    public int GetNumEnemigos()
+    {
+        return maxEnemigos;
+    }
+
+
+    // Mueve todos los enemigos en bloque y (quiza) lanza ataques
+    public Enemigo GetEnemigo(int i)
+    {
+        return enemigos[i];
+    }
+
+
 } /* fin de la clase Flota */
