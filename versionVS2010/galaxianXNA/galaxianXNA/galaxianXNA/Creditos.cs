e8b7c90a8044ee583e68f37d47b616c39ec6cb2a
ncabanes <ncabanes@25849b62-e80a-09ad-24df-e5005423cf44>
2011-02-14 13:43:18 +0000

    Esqueleo básico con XNA

---
+++ versionVS2010/galaxianXNA/galaxianXNA/galaxianXNA/Creditos.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace galaxianXNA
+{
+    class Creditos
+    {
+        GraphicsDeviceManager graphics;
+        ContentManager contenido;
+        Texture2D imagenFondo;
+        SpriteFont fuente18;
+        int xTexto = 320, yTexto = 500;
+        int incrX = 1, incrY = 1;
+
+        // Opciones posibles
+        private bool terminada;
+
+        public Creditos(GraphicsDeviceManager dispositivo, ContentManager c)
+        {
+            graphics = dispositivo;
+            contenido = c;
+        }
+
+        public void LoadContent()
+        {
+            imagenFondo = contenido.Load<Texture2D>("galaxian_logo");
+            fuente18 = contenido.Load<SpriteFont>("Lucida Console");
+        }
+
+
+        public void Update()
+        {
+            // Mover elementos (texto)
+            xTexto += incrX;
+            yTexto += incrY;
+
+            if ((xTexto < 100) || (xTexto > 380)) incrX = -incrX;
+            if ((yTexto < 500) || (yTexto > 560)) incrY = -incrY;
+
+            // Colisiones: nada por ahora
+
+            // Comprobacion de teclas para salir
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                terminada = true;
+        }
+
+
+        public void DibujarElementos(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(imagenFondo, new Vector2(100, 100), Color.White);
+            spriteBatch.DrawString(fuente18, "Remake 2011 por 1 DAI San Vicente",
+                   new Vector2(xTexto, yTexto), Color.WhiteSmoke);
+        }
+
+
+        public bool GetTerminada()
+        {
+            return terminada;
+        }
+
+
+        public void Reiniciar()
+        {
+            terminada = false;
+        }
+
+
+    }
+}
---
+++ versionVS2010/galaxianXNA/galaxianXNA/galaxianXNA/Nave.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+/** 
+*   Nave: la nave que maneja el jugador
+* 
+*   Parte de "galaxian-remake"
+*  
+*   @see Hardware
+*   @author 1-DAI IES San Vicente 2010/11
+*/
+
+/* --------------------------------------------------         
+   Versiones hasta la fecha:
+   
+   Num.   Fecha       Por / Cambios
+   ---------------------------------------------------
+   0.01  02-Feb-2011  Nacho Cabanes
+                      Version inicial: esqueleto vacio
+   0.02  03-Feb-2011  Frank Royo y Aitor Salgado
+                      Implementar los metodos que componen la clase. Hemos creado nuevos atributos
+                       ( VidasNave,VelDisparo) y un método GetVida que devolvera a partida la
+                       cantidad de vidas restantes.
+   0.03  08-Feb-2011  Maria Navarro y Julio Antoranz
+                      Mejorar disparo de nave.
+ ---------------------------------------------------- */
+
+
+
+namespace galaxianXNA
+{
+
+    public class Nave : ElemGrafico
+    {
+        //private DisparoPersonaje miDisparo;
+        private byte vidas;
+        const int VelDisparo = 5;
+
+
+        public Nave(ContentManager c)
+            : base("nave", c)
+        {
+            ancho = 39;
+            alto = 48;
+            //CargarImagen("imagenes/nave.png");
+            MoverA(400, 600 - (alto + 20));
+            //miDisparo = new DisparoPersonaje();
+            vidas = 3;
+            incrX = 5;
+        }
+
+
+        public void MoverDerecha()
+        {
+            x += incrX;
+        }
+
+
+        public void MoverIzquierda()
+        {
+            x -= incrX;
+        }
+
+        // Devuelve la posición donde sale el disparo y con el ancho indicamos la posición donde sale.
+        public void Disparar()
+        {
+            /* TODO: Falta que se actualize el documento para que no haga falta cambiar la X
+            miDisparo.Aparecer(x + ancho / 2, y, VelDisparo);*/
+
+            /*TODO: Hacer que el disaro vuelva a aparecer*/
+
+            //if (!miDisparo.GetVisible()) //mientras no este visible 
+            {
+                //miDisparo.Aparecer(x + ancho / 2, y - 5, 0, -VelDisparo);
+            }
+        }
+
+
+        public void Morir()
+        {
+            vidas--;
+        }
+
+        // Método que añade una vida adicional.
+        public void GanarVida()
+        {
+            vidas++;
+        }
+
+        // Método que devuelve la cantidad de vida restantes.
+        public byte GetVidas()
+        {
+            return vidas;
+        }
+
+        /*public DisparoPersonaje GetDisparo()
+        {
+            return miDisparo;
+        }*/
+
+    } /* fin de la clase Nave */
+
+}
